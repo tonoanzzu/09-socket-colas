6a05fea52b3030ab2f9433b529ca816984b5665d
tonoanzzu <tono.anzzu@gmail.com>
2019-08-02 15:31:06 -0500

    ultimo commit, capitulo 193 conectando la asignacionde tickets con pantalla de cola

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -10,11 +10,14 @@ class TicketControl {
         this.ultimo = 0;
         this.hoy = new Date().getDate();
         this.tickets = [];
+        this.ultimos4 = [];
 
         let data = require('../data/data.json');
         console.log(data)
         if (data.hoy === this.hoy) {
             this.ultimo = data.ultimo;
+            this.tickets = data.tickets;
+            this.ultimos4 = data.ultimos4;
         } else {
             this.reiniciarConteo();
         }
@@ -34,10 +37,40 @@ class TicketControl {
         return `Ticket ${this.ultimo}`;
     }
 
+    getUltimos4() {
+        return this.ultimos4;
+    }
+
+    atenderTicket(escritorio) {
+        if (this.tickets.length === 0) {
+            return 'No hay mas tickets'
+        }
+
+        let numeroTicket = this.tickets[0].numero;
+        this.tickets.shift();
+
+        let atenderTicket = new Ticket(numeroTicket, escritorio);
+
+        this.ultimos4.unshift(atenderTicket);
+
+        if (this.ultimos4.length > 4) {
+            this.ultimos4.splice(-1, 1);
+        }
+
+        console.log('Ultimos 4');
+        console.log(this.ultimos4);
+
+        this.grabarArchivo();
+
+        return atenderTicket;
+
+    }
+
 
     reiniciarConteo() {
         this.ultimo = 0;
         this.tickets = [];
+        this.ultimos4 = [];
         console.log('se ha reinicializado el sistema');
         this.grabarArchivo();
 
@@ -47,7 +80,8 @@ class TicketControl {
         let jsonData = {
             ultimo: this.ultimo,
             hoy: this.hoy,
-            tickets: this.tickets
+            tickets: this.tickets,
+            ultimos4: this.ultimos4
         };
         let jsonDataString = JSON.stringify(jsonData);
         fs.writeFileSync('./server/data/data.json', jsonDataString)
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -3,8 +3,6 @@ const { TicketControl } = require('../classes/ticket-control');
 
 const ticketControl = new TicketControl();
 io.on('connection', (client) => {
-
-
     // Escuchar el cliente
     client.on('siguienteTicket', (data, callback) => {
 
@@ -16,7 +14,27 @@ io.on('connection', (client) => {
     });
 
     client.emit('estadoActual', {
-        actual: ticketControl.getUltimoTicket()
+        actual: ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
+    });
+
+    client.on('atenderTicket', (data, callback) => {
+        if (!data.escritorio) {
+            return callback({
+                err: true,
+                mensaje: 'El escritorio es necesario'
+            })
+        }
+
+        let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+
+        callback(atenderTicket);
+
+        client.broadcast.emit('ultimos4', {
+            ultimos4: ticketControl.getUltimos4()
+        })
+
+
     })
 
 });
